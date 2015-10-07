b5572dd4ad1eeb5196e325505a62bf8085165167
Nate Beaty <nate@firebellydesign.com>
2015-10-07 13:18:58 -0500

    don’t link to person if they have no bio

--- web/app/themes/scb/page-people.php
+++ web/app/themes/scb/page-people.php
@@ -15,7 +15,10 @@ $person_categories = get_terms('person_category');
   	if ($people_posts = Firebelly\PostTypes\Person\get_people(['person_category' => $person_category->slug])) {
   		echo '<ul>';
   		foreach($people_posts as $people_post) {
-  			echo '<li><a href="'.get_permalink($people_post).'">'.$people_post->post_title.'</a></li>';
+  			if (!empty($people_post->post_content))
+	  			echo '<li><a href="'.get_permalink($people_post).'">'.$people_post->post_title.'</a></li>';
+	  		else
+	  			echo '<li>'.$people_post->post_title.'</li>';
   		}
   		echo '</ul>';
   	}
