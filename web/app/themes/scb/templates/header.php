1228070981795e3d855815e3e6608c0da9ff3a5a
Matt Soria <matt.m.soria@gmail.com>
2015-11-05 14:11:27 -0600

    Adding class to site navigation

--- web/app/themes/scb/templates/header.php
+++ web/app/themes/scb/templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner" role="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><svg class="icon icon-logo"><use xlink:href="#icon-logo"/></svg><span class="sr-only"><?php bloginfo('name'); ?></span></a>
-    <nav role="navigation">
+    <nav class="site-nav" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
