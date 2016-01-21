8e3bb1eff5fb6d9e7add74f6a03d993509e02223
Nate Beaty <nate@firebellydesign.com>
2016-01-21 12:46:28 -0600

    typo

--- web/app/themes/scb/templates/article-project.php
+++ web/app/themes/scb/templates/article-project.php
@@ -1,10 +1,10 @@
 <?php 
 $location = get_post_meta($project_post->ID, '_cmb2_location', true);
 $categories = wp_get_post_terms($project_post->ID, 'project_category');
-$product_categories = '';
+$project_categories = '';
 if ($categories):
   foreach($categories as $cat)
-    $product_categories .= '<a href="'.get_term_link($cat).'">'.$cat->name."</a> /<br />";
+    $project_categories .= '<a href="'.get_term_link($cat).'">'.$cat->name."</a> /<br />";
 endif;
 ?>
 <article class="project" data-id="<?= $project_post->ID ?>">
@@ -16,8 +16,8 @@ endif;
     <?php if ($location): ?>
       <h3 class="location"><?= $location ?></h3>
     <?php endif ?>
-    <?php if ($product_categories): ?>
-      <h3 class="categories"><?= $product_categories ?></h3>
+    <?php if ($project_categories): ?>
+      <h3 class="categories"><?= $project_categories ?></h3>
     <?php endif ?>
 
     <p class="actions">
