2a964fd036fdf7c78332cdc08c2be26e90886fea
Matt Soria <matt.m.soria@gmail.com>
2016-02-18 13:23:00 -0600

    Changing project thumbnail to large size

--- web/app/themes/scb/single-project.php
+++ web/app/themes/scb/single-project.php
@@ -12,7 +12,7 @@ endif;
 ?>
 <article class="project <?= $orientation ?>" data-id="<?= $post->ID ?>">
   <div class="actions">
-  <?php if (\Firebelly\Collections\post_in_collection($collection,$post->ID)): ?>
+  <?php if (\Firebelly\Collections\post_in_collection($collection,$post->ID, 'large')): ?>
     <a href="#" class="collection-action collection-remove" data-action="remove" data-id="<?= $post->ID ?>"><span class="icon icon-download"><?php include(get_template_directory().'/assets/svgs/icon-download.svg'); ?></span><span class="icon icon-remove"><?php include(get_template_directory().'/assets/svgs/icon-remove.svg'); ?></span> <span class="sr-only">Remove from Collection</span></a>
   <?php else: ?>
     <a href="#" class="collection-action collection-add" data-action="add" data-id="<?= $post->ID ?>"><span class="icon icon-download"><span class="icon icon-download"><?php include(get_template_directory().'/assets/svgs/icon-download.svg'); ?></span><span class="icon icon-remove"><?php include(get_template_directory().'/assets/svgs/icon-remove.svg'); ?></span> <span class="sr-only">Add to Collection</span></a>
