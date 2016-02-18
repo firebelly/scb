a047d1b741d183e214ddd549806bfb9c6a798606
Matt Soria <matt.m.soria@gmail.com>
2016-02-18 15:08:55 -0600

    only showing categories with results in search page

--- web/app/themes/scb/search.php
+++ web/app/themes/scb/search.php
@@ -12,33 +12,32 @@
   ];
 
   foreach ($search_arr as $cat_slug => $cat_title) {
-    echo '<div class="search-column"><h2>'.$cat_title.'</h2>';
+
     $project_posts = \Firebelly\PostTypes\Project\get_projects([
       'category' => $cat_slug,
       'search' => get_search_query(),
     ]);
+
     if ($project_posts) {
+      echo '<div class="search-column"><h2>'.$cat_title.'</h2>';
       foreach ($project_posts as $project_post) {
         include(locate_template('templates/article-project.php'));
       }
-    } else {
-      echo '<p>Nothing found.</p>';
+      echo '</div>';
     }
-    echo '</div>';
   }
   ?>
   <div class="search-column">
-    <h2>News</h2>
 
     <?php 
     $count = 0;
+    if (have_posts()) { echo '<h2>News</h2>';}
     while (have_posts()) : the_post();
       if ($post->post_type=='post') {
         $count++;
         get_template_part('templates/content', 'search');
       }
-    endwhile; 
-    if ($count==0) echo '<p>Nothing found.</p>';
+    endwhile;
     ?>
   </div>
 
