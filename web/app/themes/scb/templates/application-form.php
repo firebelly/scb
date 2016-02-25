6db54670431371df50fcff440992c9cc12275074
Nate Beaty <nate@firebellydesign.com>
2016-02-25 13:28:05 -0600

    add Send Us Your Portfolio header if not for a particular position, fieldset for styling

--- web/app/themes/scb/templates/application-form.php
+++ web/app/themes/scb/templates/application-form.php
@@ -1,19 +1,25 @@
 <form action="<?= admin_url('admin-ajax.php') ?>" class="application-form" method="post" enctype="multipart/form-data" novalidate>
-  
-  <div><input type="text" name="application_first_name" placeholder="First Name" required></div>
-  <div><input type="text" name="application_last_name" placeholder="Last Name" required></div>
-  <div><input type="email" name="application_email" placeholder="Email Address" required></div>
-  <div><input type="text" name="application_phone" placeholder="Phone Number" required></div>
 
-  <?php if (!empty($position_id)): ?>
-    <input type="hidden" name="position_id" value="<?= $position_id ?>">
+  <?php if (empty($position_id)): ?>
+    <h2>Send Us Your Portfolio</h2>
   <?php endif ?>
 
-  <input type="hidden" name="application_type" value="<?= !empty($application_type) ? $application_type : !empty($position_id) ? 'position' : 'portfolio' ?>">
-  <input name="action" type="hidden" value="application_submission">
+  <fieldset>
+    <div><input type="text" name="application_first_name" placeholder="First Name" required></div>
+    <div><input type="text" name="application_last_name" placeholder="Last Name" required></div>
+    <div><input type="email" name="application_email" placeholder="Email Address" required></div>
+    <div><input type="text" name="application_phone" placeholder="Phone Number" required></div>
 
-  <input type="file" name="application_files[]" multiple>
-  <?php wp_nonce_field( 'application_form', 'application_form_nonce' ); ?>
+    <?php if (!empty($position_id)): ?>
+      <input type="hidden" name="position_id" value="<?= $position_id ?>">
+    <?php endif ?>
+
+    <input type="hidden" name="application_type" value="<?= !empty($application_type) ? $application_type : !empty($position_id) ? 'position' : 'portfolio' ?>">
+    <input name="action" type="hidden" value="application_submission">
+
+    <input type="file" name="application_files[]" multiple>
+    <?php wp_nonce_field( 'application_form', 'application_form_nonce' ); ?>
+  </fieldset>
+
   <input type="submit" value="submit">
-
 </form>
