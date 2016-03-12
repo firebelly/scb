3e35f15ec3a83859ef8cf60ac4b3bba8fde20b58
Matt Soria <matt.m.soria@gmail.com>
2016-03-11 21:14:36 -0600

    Changing spans in actions icons for projects to divs to test ie bug

--- web/app/themes/scb/single-project.php
+++ web/app/themes/scb/single-project.php
@@ -15,9 +15,9 @@ $parent_cat = \Firebelly\Utils\get_top_parent_cat($post);
   <div class="actions">
   <a href="portfolio/" class="plus-button close single-project-close"><div class="plus"></div></a href="projects/">
   <?php if (\Firebelly\Collections\post_in_collection($collection,$post->ID, 'large')): ?>
-    <a href="#" class="collection-action collection-remove" data-action="remove" data-id="<?= $post->ID ?>"><div class="icon icon-download"><?php include(get_template_directory().'/assets/svgs/icon-download.svg'); ?></div><div class="icon icon-remove"><?php include(get_template_directory().'/assets/svgs/icon-remove.svg'); ?></div> <div class="sr-only">Remove from Collection</div></a>
+    <a href="#" class="collection-action collection-remove" data-action="remove" data-id="<?= $post->ID ?>"><span class="icon icon-download"><?php include(get_template_directory().'/assets/svgs/icon-download.svg'); ?></span><span class="icon icon-remove"><?php include(get_template_directory().'/assets/svgs/icon-remove.svg'); ?></span> <span class="sr-only">Remove from Collection</span></a>
   <?php else: ?>
-    <a href="#" class="collection-action collection-add" data-action="add" data-id="<?= $post->ID ?>"><span class="icon icon-download"><span class="icon icon-download"><?php include(get_template_directory().'/assets/svgs/icon-download.svg'); ?></span><span class="icon icon-remove"><?php include(get_template_directory().'/assets/svgs/icon-remove.svg'); ?></span> <span class="sr-only">Add to Collection</span></a>
+    <a href="#" class="collection-action collection-add" data-action="add" data-id="<?= $post->ID ?>"><span class="icon icon-download"><?php include(get_template_directory().'/assets/svgs/icon-download.svg'); ?></span><span class="icon icon-remove"><?php include(get_template_directory().'/assets/svgs/icon-remove.svg'); ?></span> <span class="sr-only">Add to Collection</span></a>
   <?php endif; ?>
   </div>
 
