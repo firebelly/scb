770ec9620266d0667bc7adb290edcb80b7139a3f
Nate Beaty <nate@firebellydesign.com>
2016-03-22 16:44:15 -0500

    wrap news excerpt in paragraph

--- web/app/themes/scb/templates/article-news-excerpt.php
+++ web/app/themes/scb/templates/article-news-excerpt.php
@@ -14,7 +14,7 @@ $featured_class = get_post_meta($news_post->ID, '_featured', true) ? ' featured-
         <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?= wp_trim_words($news_post->post_title, 10); ?></a></h2>
       </header>
       <div class="entry-summary">
-        <?= Utils\get_excerpt($news_post); ?>
+        <p><?= Utils\get_excerpt($news_post); ?></p>
         <a href="<?php the_permalink(); ?>" class="read-more-link">+ <span class="sr-only">Continued</span></a>
       </div>
     </div>
