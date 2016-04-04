1910e9d1f21fe60cfb77c3f73aeceabf947b8f88
Nate Beaty <nate@firebellydesign.com>
2016-04-04 11:32:34 -0500

    add webmaster tool tag

--- web/app/themes/scb/templates/head.php
+++ web/app/themes/scb/templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="PyphqoxtKzqR2DEtVOLTrlmjAXUIARNbqf1DbVcVFxQ" />
   <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png') ?>" />
   <?php wp_head(); ?>
 </head>
