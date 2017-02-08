a7e29b88e3a9dff9c06552f518cee3ed7c0581a1
Nate Beaty <nate@firebellydesign.com>
2017-02-08 15:04:41 -0600

    update browser sync

--- web/app/themes/scb/base.php
+++ web/app/themes/scb/base.php
@@ -46,8 +46,8 @@ if (is_tax()) {
       wp_footer();
     ?>
     <?php if (WP_ENV === 'development'): ?>
-    <script type='text/javascript' id="__bs_script__">//<![CDATA[
-        document.write("<script async src='http://HOST:3002/browser-sync/browser-sync-client.2.11.1.js'><\/script>".replace("HOST", location.hostname));
+    <script id="__bs_script__">//<![CDATA[
+        document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.12.5.js'><\/script>".replace("HOST", location.hostname));
     //]]></script>
     <?php endif; ?>
   </body>
