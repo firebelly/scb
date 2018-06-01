b3da565ff9a9a873b26f809b88ada6ecd3a2af8a
Nate Beaty <nate@clixel.com>
2018-05-31 22:24:38 -0500

    update wp & plugins, switch to ssl on production

--- config/environments/production.php
+++ config/environments/production.php
@@ -3,4 +3,6 @@
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
+define('FORCE_SSL_ADMIN', true);
+define('FORCE_SSL_LOGIN', true);
 // define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
