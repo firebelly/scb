a850bb82d1298151646fc354defa88bde13eaeef
Nate Beaty <nate@clixel.com>
2021-03-16 11:10:47 -0400

    fix vcard uploads

--- web/app/themes/scb/lib/fb_assets.php
+++ web/app/themes/scb/lib/fb_assets.php
@@ -54,7 +54,7 @@ function admin_scripts_inline() {
 // Allow vCard uploads
 add_filter('upload_mimes', __NAMESPACE__ . '\allow_vcard_upload');
 function allow_vcard_upload ( $mimes=array() ){
-  $mimes['vcf'] = 'text/x-vcard';
+  $mimes['vcf'] = 'text/vcard';
   $mimes['vcard'] = 'text/vcard';
   return $mimes;
 }
